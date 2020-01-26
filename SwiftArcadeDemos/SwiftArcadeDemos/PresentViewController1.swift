f303930eeaea8da1e04f0667cd80b59b7d57e6cb
jrasmusson <rasmus4200@gmail.com>
2020-01-26 08:38:57 -0700

    cleanup

---
+++ SwiftArcadeDemos/SwiftArcadeDemos/PresentViewController1.swift
@@ -0,0 +1,17 @@
+//
+//  PresentViewController1.swift
+//  SwiftArcadeDemos
+//
+//  Created by Jonathan Rasmusson Work Pro on 2020-01-26.
+//  Copyright © 2020 Rasmusson Software Consulting. All rights reserved.
+//
+
+import UIKit
+
+class PresentViewController1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
--- SwiftArcadeDemos/SwiftArcadeDemos/UIViewControllerDemo.swift
+++ SwiftArcadeDemos/SwiftArcadeDemos/UIViewControllerDemo.swift
@@ -10,13 +10,29 @@ import UIKit
 
 class UIViewControllerDemo: UIViewController {
     
+    let presentButton: UIButton = {
+        let button = makeButton(withText: "Present")
+        button.addTarget(self, action: #selector(buttonPressed), for: .primaryActionTriggered)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
     }
     
     func setupViews() {
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .white
         navigationItem.title = "UIViewController"
+        
+        view.addSubview(presentButton)
+        
+        presentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        presentButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    // MARK: - Actions
+    @objc func buttonPressed() {
+        // create & present a new viewController
     }
 }
