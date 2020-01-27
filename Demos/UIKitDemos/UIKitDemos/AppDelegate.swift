38f2d929543cbc462664c80a35f5a7fc5cba41ae
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2020-01-27 10:25:46 -0700

    cleanup

---
+++ Demos/UIKitDemos/UIKitDemos/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  UIKitDemos
+//
+//  Created by Jonathan Rasmusson (Contractor) on 2020-01-27.
+//  Copyright © 2020 Jonathan Rasmusson. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+
+        return true
+    }
+
+}
+
