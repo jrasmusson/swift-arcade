03d8bf60e83040f7cdbfe016537d88c21953c6e9
Jonathan Rasmusson <jonathan.rasmusson@sjrb.ca>
2020-03-11 08:17:56 -0600

    cleanup

--- Source/DemoArcade/DemoArcade/AppDelegate.swift
+++ Source/DemoArcade/DemoArcade/AppDelegate.swift
@@ -22,6 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let tableViewPatterns = [
                 Lab(name: "Simple View Controller", viewController: aSimpleUITableViewController()),
                 Lab(name: "Simple View", viewController: aSimpleUITableView()),
+                Lab(name: "Edit Mode", viewController: InsertDeletingRowsEditMode()),
                 Lab(name: "Swiping Cells", viewController: SwipingCellsTableViewController()),
             ]
 
---
+++ Source/DemoArcade/DemoArcade/InsertDeletingRowsEditMode.swift
@@ -0,0 +1,136 @@
+//
+//  InsertDeletingRowsEditMode.swift
+//  DemoArcade
+//
+//  Created by Jonathan Rasmusson (Contractor) on 2020-03-11.
+//  Copyright © 2020 Rasmusson Software Consulting. All rights reserved.
+//
+
+import UIKit
+
+class InsertDeletingRowsEditMode: UIViewController {
+
+    var games = ["Space Invaders",
+                "Dragon Slayer",
+                "Disks of Tron",
+                "Moon Patrol",
+                "Galaga"]
+
+    let textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.font = UIFont.preferredFont(forTextStyle: .body)
+        textField.textAlignment = .center
+        textField.backgroundColor = .systemFill
+
+        return textField
+    }()
+
+    lazy var addButton: UIButton = {
+        let button = makeButton(withText: "Add")
+        button.setContentHuggingPriority(UILayoutPriority.defaultHigh, for: .horizontal)
+        button.addTarget(self, action: #selector(addButtonPressed), for: .primaryActionTriggered)
+
+        return button
+    }()
+
+    var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        return tableView
+    }()
+
+    let cellId = "insertCellId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        layout()
+    }
+
+    func setupViews() {
+        navigationItem.title = "Classic Arcade"
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+
+    func layout() {
+        let addStackView = makeHorizontalStackView()
+        addStackView.addArrangedSubview(textField)
+        addStackView.addArrangedSubview(addButton)
+
+        view.addSubview(addStackView)
+        view.addSubview(tableView)
+
+        addStackView.topAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.topAnchor, multiplier: 3).isActive = true
+        addStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3).isActive = true
+        view.trailingAnchor.constraint(equalToSystemSpacingAfter: addStackView.trailingAnchor, multiplier: 3).isActive = true
+
+        tableView.topAnchor.constraint(equalToSystemSpacingBelow: addStackView.bottomAnchor, multiplier: 1).isActive = true
+        tableView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1).isActive = true
+        view.trailingAnchor.constraint(equalToSystemSpacingAfter: tableView.trailingAnchor, multiplier: 1).isActive = true
+        view.safeAreaLayoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: tableView.bottomAnchor, multiplier: 1).isActive = true
+    }
+
+    // MARK: - Actions
+
+    // 3. Custom target action
+    @objc
+    func addButtonPressed() {
+        tableView.isEditing = true
+    }
+
+    private func insertTextBottomRow() {
+        guard let text = textField.text else { return }
+        games.append(text)
+
+        let indexPath = IndexPath(row: games.count - 1, section: 0)
+
+        tableView.beginUpdates()
+        tableView.insertRows(at: [indexPath], with: .fade)
+        tableView.endUpdates()
+    }
+}
+
+// MARK:  - UITableView Delegate
+
+extension InsertDeletingRowsEditMode: UITableViewDelegate {
+
+}
+
+// MARK:  - UITableView DataSource
+
+extension InsertDeletingRowsEditMode: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+
+        cell.textLabel?.text = games[indexPath.row]
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+
+        if editingStyle == .delete {
+            games.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+}
+
