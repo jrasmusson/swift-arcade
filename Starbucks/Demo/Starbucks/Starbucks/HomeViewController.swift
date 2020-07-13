4b209d9e9179e09d4cfdbc411f5970c2e2897cf8
jrasmusson <rasmus4200@gmail.com>
2020-07-13 08:10:52 -0600

    Demo including tiles

--- Starbucks/Demo/Starbucks/Starbucks/HomeViewController.swift
+++ Starbucks/Demo/Starbucks/Starbucks/HomeViewController.swift
@@ -11,19 +11,23 @@ import UIKit
 class HomeViewController: StarBucksViewController {
 
     let headerView = HomeHeaderView()
+    let scrollView = UIScrollView()
+    let stackView = UIStackView()
+    
     var headerViewTopConstraint: NSLayoutConstraint?
     
     let tiles = [
-                "Star balance",
-                "Bonus stars",
-                "Try these",
-                "Welcome back",
-                "Uplifting"
+                TileView("Star balance"),
+                TileView("Bonus stars"),
+                TileView("Try these"),
+                TileView("Welcome back"),
+                TileView("Uplifting")
     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupNavBar()
+        setupScrollView()
         setTabBarImage(imageName: "house.fill", title: "Home")
         
         style()
@@ -33,16 +37,35 @@ class HomeViewController: StarBucksViewController {
     func setupNavBar() {
         navigationController?.navigationBar.topItem?.title = "Good afternoon, Jonathan ☀️"
     }
+    
+    func setupScrollView() {
+        scrollView.delegate = self
+    }
 }
 
 extension HomeViewController {
     func style() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
         headerView.backgroundColor = .systemPink
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.axis = .vertical
+        stackView.spacing = 8
     }
     
     func layout() {
         view.addSubview(headerView)
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(stackView)
+        
+        for tile in tiles {
+            addChild(tile)
+            stackView.addArrangedSubview(tile.view)
+            tile.didMove(toParent: self)
+        }
         
         headerViewTopConstraint = headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
 
@@ -50,6 +73,17 @@ extension HomeViewController {
             headerViewTopConstraint!,
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            scrollView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 8),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
         ])
     }
 }
@@ -58,21 +92,22 @@ extension HomeViewController {
 
 // MARK: Animating scrollView
 
+extension HomeViewController: UIScrollViewDelegate {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let y = scrollView.contentOffset.y
         
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        let y = scrollView.contentOffset.y
-//
-//        let swipingDown = y <= 0
-//        let shouldSnap = y > 30
-//        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
-//
-//        UIView.animate(withDuration: 0.3) {
-//            self.headerView.greeting.alpha = swipingDown ? 1.0 : 0.0
-//        }
-//
-//        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: [], animations: {
-//            self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
-//            self.view.layoutIfNeeded()
-//        })
-//    }
-
+        let swipingDown = y <= 0
+        let shouldSnap = y > 30
+        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.headerView.greeting.alpha = swipingDown ? 1.0 : 0.0
+        }
+        
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: [], animations: {
+            self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
+            self.view.layoutIfNeeded()
+        })
+    }
+}
