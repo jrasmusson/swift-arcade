172265fd76a93666b3e0aa7d020a04ca63eaab42
jrasmusson <rasmus4200@gmail.com>
2020-07-20 16:13:28 -0600

    Working on table view

--- Starbucks/Spikes/8-JSON-Tables/HistorySpike/HistorySpike/HistoryViewController.swift
+++ Starbucks/Spikes/8-JSON-Tables/HistorySpike/HistorySpike/HistoryViewController.swift
@@ -22,45 +22,113 @@ struct Transaction: Codable {
     }
 }
 
+struct HistorySection {
+    let title: String
+    let transactions: [Transaction]
+}
+
+struct HistoryViewModel {
+    let sections: [HistorySection]
+}
+
 class HistoryViewController: UITableViewController {
     
-    let games = ["Pacman",
-                 "Space Invaders",
-                 "Space Patrol",
-                 "Galaga",
-                 "Donkey Kong"]
-    
     let cellId = "cellId"
-    
+    var viewModel: HistoryViewModel?
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        data()
         style()
+    }
+    
+    func data() {
+        let tx1 = Transaction(id: 1, type: "redeemed", amount: "1", date: Date())
+        let tx11 = Transaction(id: 1, type: "redeemed", amount: "11", date: Date())
+
+        let tx2 = Transaction(id: 1, type: "redeemed", amount: "2", date: Date())
+
+        let tx3 = Transaction(id: 1, type: "redeemed", amount: "3", date: Date())
+        let tx33 = Transaction(id: 1, type: "redeemed", amount: "33", date: Date())
+        let tx333 = Transaction(id: 1, type: "redeemed", amount: "333", date: Date())
+        
+        let section1 = HistorySection(title: "July", transactions: [tx1, tx11])
+        let section2 = HistorySection(title: "June", transactions: [tx2])
+        let section3 = HistorySection(title: "May", transactions: [tx3, tx33, tx333])
+        
+        viewModel = HistoryViewModel(sections: [section1, section2, section3])
     }
     
     func style() {
         navigationItem.title = "History"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
     }
-    
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-    }
 }
 
 // MARK: Data Source
+
 extension HistoryViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        guard let vm = viewModel else { return UITableViewCell() }
         
-        cell.textLabel?.text = games[indexPath.row]
-        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let section = indexPath.section
+        
+        // limit of x3
+        var text: String
+        switch section {
+        case 0:
+            text = vm.sections[0].transactions[indexPath.row].amount
+        case 1:
+            text = vm.sections[1].transactions[indexPath.row].amount
+        case 2:
+            text = vm.sections[2].transactions[indexPath.row].amount
+        default:
+            text = ""
+        }
+
+        cell.textLabel?.text = text
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return games.count
+        guard let vm = viewModel else { return 0 }
+    
+        switch section {
+        case 0:
+            return vm.sections[0].transactions.count
+        case 1:
+            return vm.sections[1].transactions.count
+        case 2:
+            return vm.sections[2].transactions.count
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let vm = viewModel else { return nil }
+        
+        switch section {
+        case 0:
+            return vm.sections[0].title
+        case 1:
+            return vm.sections[1].title
+        case 2:
+            return vm.sections[2].title
+        default:
+            return nil
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 60
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        guard let sections = viewModel?.sections else { return 0 }
+        return sections.count
     }
 }
