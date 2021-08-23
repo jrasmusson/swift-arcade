e3db48d46fc46e84a01711e6aa6434cbc355ba7d
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-08-23 06:39:27 -0600

    cleanup

--- UIKIt/UITableView/FullPowerTableView/FullPowerTableView/HeaderView/HeaderView.swift
+++ UIKIt/UITableView/FullPowerTableView/FullPowerTableView/HeaderView/HeaderView.swift
@@ -12,10 +12,29 @@ class HeaderView: UIView {
     
     @IBOutlet var contentView: UIView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        let bundle = Bundle.init(for: HeaderView.self)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 104)
+    }
+
+    private func commonInit() {
+        let bundle = Bundle(for: HeaderView.self)
         bundle.loadNibNamed("HeaderView", owner: self, options: nil)
         addSubview(contentView)
+
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
     }
 }
--- UIKIt/UITableView/FullPowerTableView/FullPowerTableView/ViewController.swift
+++ UIKIt/UITableView/FullPowerTableView/FullPowerTableView/ViewController.swift
@@ -15,31 +15,55 @@ class ViewController: UIViewController {
         "Space Patrol",
     ]
     
+    @IBOutlet var tableView: UITableView!
+    
     let cellId = "cellId"
-
-    var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
+        setup()
     }
 
-    func setupViews() {
+}
+
+// MARK: - Setup
+extension ViewController {
+    func setup() {
+        setupTableView()
+        setupTableViewHeader()
+    }
+    
+    private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
 
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-
         tableView.tableFooterView = UIView() // hide empty rows
+    }
+    
+    private func setupTableViewHeader() {
+        let header = HeaderView(frame: .zero)
 
-        view = tableView
+        // Set frame size before populate view to have initial size
+        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+
+        // Recalculate header size after populated with content
+        size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+
+        tableView.tableHeaderView = header
     }
 }
 
+// MARK: - UITableViewDelegate
 extension ViewController: UITableViewDelegate {
 
 }
 
+// MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
