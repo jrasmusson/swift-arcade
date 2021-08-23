d39a167165ee5d3c2ad54a00615f55352c2c72ac
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-08-23 05:40:15 -0600

    cleanup

--- UIKIt/UITableView/FullPowerTableView/FullPowerTableView/ViewController.swift
+++ UIKIt/UITableView/FullPowerTableView/FullPowerTableView/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let games = [
+        "Pacman",
+        "Space Invaders",
+        "Space Patrol",
+    ]
+    
+    let cellId = "cellId"
+
+    var tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        setupViews()
     }
 
+    func setupViews() {
+        tableView.delegate = self
+        tableView.dataSource = self
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+
+        tableView.tableFooterView = UIView() // hide empty rows
+
+        view = tableView
+    }
+}
+
+extension ViewController: UITableViewDelegate {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+
+        cell.textLabel?.text = games[indexPath.row]
+        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
+
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+    }
+}
