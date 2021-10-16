07dc735ba9f42ecf756e7cac56619074210db45e
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-10-16 07:41:12 -0600

    cleanup

--- Swift/Currency-Formatting/CurrencyFormatter/CurrencyFormatter/AppDelegate.swift
+++ Swift/Currency-Formatting/CurrencyFormatter/CurrencyFormatter/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = ViewController2()
+        window?.rootViewController = ViewController()
         
         return true
     }
--- Swift/Currency-Formatting/CurrencyFormatter/CurrencyFormatter/ViewController.swift
+++ Swift/Currency-Formatting/CurrencyFormatter/CurrencyFormatter/ViewController.swift
@@ -24,7 +24,6 @@ class ViewController: UIViewController {
         return ""
     }
     
-    let stackView = UIStackView()
     let label = UILabel()
     
     override func viewDidLoad() {
@@ -36,23 +35,17 @@ class ViewController: UIViewController {
 
 extension ViewController {
     func style() {
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 20
-        
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = amountFormatted
         label.font = UIFont.preferredFont(forTextStyle: .title1)
     }
     
     func layout() {
-        stackView.addArrangedSubview(label)
-        
-        view.addSubview(stackView)
+        view.addSubview(label)
         
         NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
     }
 }
