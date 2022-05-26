3753d920b04c334b879a050fa7f4a1865c67932d
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-05-26 07:38:59 -0600

    cleanup

---
+++ SampleApps/Kijiji/Kijiji/Kijiji/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Kijiji
+//
+//  Created by jrasmusson on 2022-05-26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+
+        return true
+    }
+
+}
+
---
+++ SampleApps/Kijiji/Kijiji/Kijiji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kijiji
+//
+//  Created by jrasmusson on 2022-05-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
