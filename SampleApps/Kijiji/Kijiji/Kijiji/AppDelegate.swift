5384a5f4892ce4d9a5bbfd2bd4857b9362f1b268
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-05-27 07:46:56 -0600

    cleanup

--- SampleApps/Kijiji/Kijiji/Kijiji/AppDelegate.swift
+++ SampleApps/Kijiji/Kijiji/Kijiji/AppDelegate.swift
@@ -12,25 +12,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    let mainViewController = MainViewController()
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
 
-        let homeVC = HomeViewController()
-        let myVC = MyKijijiViewController()
-
-        homeVC.setTabBarItem(imageName: "house", title: "Home")
-        myVC.setTabBarItem(imageName: "person", title: "My Kijiji")
-
-        let tabBarController = UITabBarController()
-
-        tabBarController.viewControllers = [homeVC, myVC]
-        tabBarController.tabBar.tintColor = .systemGreen
-        tabBarController.tabBar.isTranslucent = false
-
-        window?.rootViewController = tabBarController
+        window?.rootViewController = mainViewController
 
         return true
     }
