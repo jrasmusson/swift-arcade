3e8e5b28ec3fb1c9799df83d7c987330fd4e0785
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-05-28 06:55:08 -0600

    cleanup

--- SampleApps/Kijiji/Kijiji/Kijiji/Home/SearchBarView.swift
+++ SampleApps/Kijiji/Kijiji/Kijiji/Home/SearchBarView.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SearchBarView: UIView {
 
+    let stackView = UIStackView()
+    let textField = UITextField()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -31,9 +34,26 @@ extension SearchBarView {
     func style() {
         translatesAutoresizingMaskIntoConstraints = false
         backgroundColor = .systemGreen
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .systemMint
+
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.backgroundColor = .systemRed
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Search for anything..."
     }
 
     func layout() {
+        stackView.addArrangedSubview(textField)
 
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
     }
 }
