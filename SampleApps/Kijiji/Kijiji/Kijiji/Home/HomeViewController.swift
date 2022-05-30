637214de98f98f8521f199e00388de6a2db24ae6
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-05-30 15:24:09 -0600

    cleanup

--- SampleApps/Kijiji/Kijiji/Kijiji/Home/HomeViewController.swift
+++ SampleApps/Kijiji/Kijiji/Kijiji/Home/HomeViewController.swift
@@ -13,10 +13,22 @@ class HomeViewController: UIViewController {
     let searchBarView = SearchBarView()
     let categoryView = CategoryView()
 
+    enum Section {
+        case main
+    }
+
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
+
+        // CollectionView
+        navigationItem.title = "Two-Column Grid"
+        configureHierarchy()
+        configureDataSource()
     }
 }
 
@@ -52,5 +64,109 @@ struct HomeViewController_Preview: PreviewProvider {
         UIViewControllerPreview {
             HomeViewController()
         }
+    }
+}
+
+// MARK: - CollectionView
+
+extension HomeViewController {
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                             heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        //
+        // Here we are saying make me two columns. Horizontal count: 2.
+        // Even though the itemSize say - make me 1:1, the group layout overrides that and makes
+        // it stretch to something longer. So group overrides item.
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .absolute(44))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+        let spacing = CGFloat(10)
+        group.interItemSpacing = .fixed(spacing)
+
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = spacing
+
+        // Another way to add spacing. This is done for the section.
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+    }
+
+    func configureHierarchy() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(TextCell.self, forCellWithReuseIdentifier: TextCell.reuseIdentifier)
+        view.addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: categoryView.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: collectionView.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+
+            // Get a cell of the desired kind.
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: TextCell.reuseIdentifier,
+                for: indexPath) as? TextCell else { fatalError("Cannot create new cell") }
+
+            // Populate the cell with our item description.
+            cell.label.text = "\(identifier)"
+            cell.contentView.backgroundColor = .systemBlue
+            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderWidth = 1
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+
+            // Return the cell.
+            return cell
+        }
+
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(Array(0..<20))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+class TextCell: UICollectionViewCell {
+    let label = UILabel()
+    static let reuseIdentifier = "text-cell-reuse-identifier"
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemnted")
+    }
+
+}
+
+extension TextCell {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        label.font = UIFont.preferredFont(forTextStyle: .caption1)
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset)
+            ])
     }
 }
