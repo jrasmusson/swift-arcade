daff417bce245ef0a4a06bb79bf2a93273b22d98
Jonathan Rasmusson <rasmus4200@gmail.com>
2022-07-18 06:51:21 -0600

    cleanup

---
+++ Spotify/easyplay1/easyplay1/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  easyplay1
+//
+//  Created by jrasmusson on 2022-07-18.
+//
+
+import Foundation
+
+let accessTokenKey = "access-token-key"
+let redirectUri = URL(string:"easyplay1://")!
+let spotifyClientId = "d6025900316c45789250da94cf635596"
+let spotifyClientSecretKey = "f8448f3f169345acb07796a36a22937b"
+
+/*
+Scopes let you specify exactly what types of data your application wants to
+access, and the set of scopes you pass in your call determines what access
+permissions the user is asked to grant.
+For more information, see https://developer.spotify.com/web-api/using-scopes/.
+*/
+let scopes: SPTScope = [
+                            .userReadEmail, .userReadPrivate,
+                            .userReadPlaybackState, .userModifyPlaybackState, .userReadCurrentlyPlaying,
+                            .streaming, .appRemoteControl,
+                            .playlistReadCollaborative, .playlistModifyPublic, .playlistReadPrivate, .playlistModifyPrivate,
+                            .userLibraryModify, .userLibraryRead,
+                            .userTopRead, .userReadPlaybackState, .userReadCurrentlyPlaying,
+                            .userFollowRead, .userFollowModify,
+                        ]
+let stringScopes = [
+                        "user-read-email", "user-read-private",
+                        "user-read-playback-state", "user-modify-playback-state", "user-read-currently-playing",
+                        "streaming", "app-remote-control",
+                        "playlist-read-collaborative", "playlist-modify-public", "playlist-read-private", "playlist-modify-private",
+                        "user-library-modify", "user-library-read",
+                        "user-top-read", "user-read-playback-position", "user-read-recently-played",
+                        "user-follow-read", "user-follow-modify",
+                    ]
